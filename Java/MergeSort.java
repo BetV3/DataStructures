1316b35b4ae7739e8904499d139820958ed934d4
Elvis Ramirez <elvisramirez999@gmail.com>
2022-12-21 00:54:00 -0600

    Added boilerplate code for MergeSort

---
+++ Java/MergeSort.java
@@ -0,0 +1,12 @@
+package Java;
+import static org.junit.Assert.assertEquals;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static int[] mergeSort(int[] array) {
+
+        return new int[1];
+    }
+}
