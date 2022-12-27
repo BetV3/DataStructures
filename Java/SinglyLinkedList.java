9d92bb2464d46ddc78805888a8dfbf59099304ef
Elvis Ramirez <elvisramirez999@gmail.com>
2022-12-27 15:12:21 -0600

    Implemented multiple add methods and get methods

--- Java/SinglyLinkedList.java
+++ Java/SinglyLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 class Node {
     int val;
     Node next;
@@ -16,6 +18,7 @@ class Node {
 
 }
 public class SinglyLinkedList {
+    int length = 0;
     Node head;
     public SinglyLinkedList() {
         this.head = new Node();
@@ -36,6 +39,7 @@ public class SinglyLinkedList {
         Node add = new Node(val);
         if (head.next == null) {
             head.next = add;
+            length++;
         }
         else {
             Node temp = head;
@@ -43,6 +47,7 @@ public class SinglyLinkedList {
                 temp = temp.next;
             }
             temp.next = add;
+            length++;
         }
     }
     public void add(int index, int value) {
@@ -52,6 +57,7 @@ public class SinglyLinkedList {
         }
         if (this.head.next == null) {
             head.next = add;
+            length++;
         }
         else {
             Node temp = this.head;
@@ -62,27 +68,116 @@ public class SinglyLinkedList {
             }
             add.next = temp.next;
             temp.next = add;
+            length++;
         }
     }
     public void addFirst(int value) {
         Node add = new Node(value);
+        // Testing edge case if LinkedList is empty
+        if(this.head == null) {
+            this.head = add;
+            length++;
+        }  
+        // Adding Node to the head
         Node temp = head;
         this.head = add;
         this.head.next = temp;
     }
+    public void addLast(int value) {
+        Node add = new Node(value);
+        if (this.head == null) {
+            this.head = add;
+        }
+        Node temp = head;
+        while(temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = add;
+    }
+    public void clear(){
+        this.head = null;
+    }
+    public int get(int index) {
+        if (index > this.size() || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            return this.head.val;
+        }
+        Node temp = this.head;
+        int cur = 0;
+        while (cur != index) {
+            temp = temp.next;
+            cur++;
+        }
+        return temp.val;
+    }
+    public int getFirst() {
+        if (this.size() == 0) {
+
+        }
+        return this.head.val;
+    }
+    public int getLast() {
+        if (this.size() == 0) {
+            throw new NoSuchElementException();
+        }
+        if (this.size() == 1) {
+            return head.val;
+        }
+        Node temp = this.head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+    public int indexOf(int value) {
+        if (this.head.val == value) {
+            return 0;
+
+        }
+        Node temp = this.head;
+        int cur = 0;
+        while (temp.next != null) {
+            if (temp.val == value) {
+                return cur; 
+            }
+            temp = temp.next;
+            cur++;
+        }
+        return -1;
+    }
+    public int lastIndexOf(int value) {
+        if (this.head.val == value) {
+            return 0;
+        }
+        Node temp = this.head;
+        int curr = 0;
+        int lastIndex = -1;
+        while (temp.next != null) {
+            if (temp.val == value) {
+                lastIndex = curr;
+            }
+            temp = temp.next;
+            curr++;
+        }
+        return lastIndex;
+    }
     public void printList() {
         Node temp = head;
         while (temp != null) {
-            System.out.println(temp.val);
+            System.out.print(String.valueOf(temp.val) + " ");
             temp = temp.next;
         }
     }
     public static void main(String[] args) {
         SinglyLinkedList testing = new SinglyLinkedList(1);
         testing.add(2);
-        testing.add(3);
-        testing.add(-1, 5);
+        testing.add(2);
+        testing.add(1, 5);
         testing.addFirst(6);
+        testing.addLast(10);
+        System.out.println(testing.lastIndexOf(1));
         testing.printList();
     }
 }
